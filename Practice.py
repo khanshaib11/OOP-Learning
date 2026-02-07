ce9f76e36f3d56b46d885c05d7feb31e8a7d95b8
khanshaib11 <khanshaib11@users.noreply.github.com>
2026-02-07 12:14:25 +0500

    Solved Question 5

--- Practice.py
+++ Practice.py
@@ -57,4 +57,39 @@ rec1 = Rectangle(length=10, width=20)
 
 print(rec1.area())
 
+"""Question 5"""
 
+class Book:
+    def __init__(self,title,author):
+        self.title = title
+        self.author = author
+    
+    def describe_book(self):
+        print(f"Book Title is {self.title} and its author is {self.author} ") 
+        
+book1 = Book(title="Acha Dost..." , author= "Bilal Ahmed khan Niazi ")  
+    
+book1.describe_book()
+
+
+"""Question 6"""
+
+class BankAccount:
+    def __init__(self,balance):
+        self.balance = balance
+    
+    def Deposit(self,amount):
+         self.balance += amount
+         
+    def Withdraw(self,amount):
+        if self.balance < amount:
+            return False
+        self.balance -= amount
+        
+    def Show_balance(self):
+        print(f"You have {self.balance} amount of balance left, have a nice day sir!!! ")
+
+bank1 = BankAccount(balance=0)
+bank1.Deposit(100)
+bank1.Withdraw(100)
+bank1.Show_balance()
